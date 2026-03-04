5de7a54a5a338409c25865261477105e1f3bb263
Luciano Siqueira Barboza <lbarboza@byupathway.edu>
2026-03-04 10:06:27 -0300

    Finished Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.Write("What is your guess?: ");
+        int guess = int.Parse(Console.ReadLine());
+        
+
+        while (guess != number)
+    {
+    if (guess < number)
+    {
+        Console.WriteLine("higher");
+    }
+    else
+    {
+        Console.WriteLine("lower");
+    }
+
+    Console.Write("What is your guess?: ");
+    guess = int.Parse(Console.ReadLine()); // ← AQUI você muda o valor
+    }
+       Console.WriteLine("Congratulations! You guessed the magic number!");
     }
 }
