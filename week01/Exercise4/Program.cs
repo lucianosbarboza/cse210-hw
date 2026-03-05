9e3b9736fc6116da33e692e643a4347de1dc31cb
Luciano Siqueira Barboza <lbarboza@byupathway.edu>
2026-03-04 23:04:03 -0300

    Exercise 4 finished

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished: ");   
+
+        Console.Write("Enter a number: ");
+        int Number = int.Parse(Console.ReadLine());
+        
+        while (Number != 0)
+        {
+            numbers.Add(Number);
+            Console.Write("Enter a number: ");
+            Number = int.Parse(Console.ReadLine());
+        }
+        
+        int total = 0;
+       
+        foreach (int num in numbers)
+        {
+            total += num;
+        }
+        
+        Console.WriteLine("The sum is: " + total);
+        
+        double average = (double)total / numbers.Count;
+        
+        Console.WriteLine("The average is: " + average);
+
+        int max = numbers.Max();
+        Console.WriteLine("The largest number is: " + max);
     }
 }
