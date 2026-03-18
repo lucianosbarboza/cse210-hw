235ce421dea7c16ddff3d69d8c20efb0e470d910
Luciano Siqueira Barboza <lbarboza@byupathway.edu>
2026-03-17 23:22:50 -0300

    minor changes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "DDS - Doctor of Dental Surgery";
+        job1._company = "Mondent Dental Practice";
+        job1._startYear = 2015;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "DDS - Doctor of Dental Surgery";
+        job2._company = "DR Barboza Dental Practice";
+        job2._startYear = 2023;
+        job2._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._name = "Luciano Barboza";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
