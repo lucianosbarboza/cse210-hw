843a9a84357086dd2fa3347d994b0c50e4d61955
Luciano Siqueira Barboza <lbarboza@byupathway.edu>
2026-07-16 20:01:38 -0300

    Upload files

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
-class Program
+namespace Fractions
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Testing Fraction Class ---");
+
+            Fraction f1 = new Fraction();
+            Console.WriteLine(f1.GetFractionString());
+            Console.WriteLine(f1.GetDecimalValue());
+
+            Fraction f2 = new Fraction(5);
+            Console.WriteLine(f2.GetFractionString());
+            Console.WriteLine(f2.GetDecimalValue());
+
+            Fraction f3 = new Fraction(3, 4);
+            Console.WriteLine(f3.GetFractionString());
+            Console.WriteLine(f3.GetDecimalValue());
+
+            Fraction f4 = new Fraction(1, 3);
+            Console.WriteLine(f4.GetFractionString());
+            Console.WriteLine(f4.GetDecimalValue());
+
+            Console.WriteLine("\n--- Testing Getters and Setters ---");
+            Fraction testFraction = new Fraction();
+            testFraction.SetTop(6);
+            testFraction.SetBottom(7);
+
+            Console.WriteLine($"New Top: {testFraction.GetTop()}");
+            Console.WriteLine($"New Bottom: {testFraction.GetBottom()}");
+            Console.WriteLine($"Resulting Fraction: {testFraction.GetFractionString()}");
+        }
     }
 }
